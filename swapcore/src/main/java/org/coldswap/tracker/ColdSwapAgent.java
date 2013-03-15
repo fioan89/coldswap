683e745ed1b1e52f9816ffd7f17d2154d5553721
Faur Ioan-Aurel <fioan89@gmail.com>
2013-03-15 14:05:51 +0200

    Initial project structure and settings.

---
+++ swapcore/src/main/java/org/coldswap/tracker/ColdSwapAgent.java
@@ -0,0 +1,29 @@
+package org.coldswap.tracker;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * (C) Copyright 2013 Faur Ioan-Aurel.
+ * <p/>
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * <p/>
+ * Contributors:
+ * faur
+ * <p/>
+ * Created at:
+ * 2:01 PM       3/15/13
+ */
+
+public class ColdSwapAgent {
+    public static void premain(String args, Instrumentation inst) {
+        System.out.println("Dummy string");
+    }
+}
